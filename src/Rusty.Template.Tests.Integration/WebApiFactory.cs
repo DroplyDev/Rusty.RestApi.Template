dd6b477603a7cbf242999d4b75a23c3fb5b2d8ea
RomanRusty <roman.yatskovyna@gmail.com>
2022-12-18 12:04:40 +0000

    Moved test settings to appsettings file

--- src/Rusty.Template.Tests.Integration/WebApiFactory.cs
+++ src/Rusty.Template.Tests.Integration/WebApiFactory.cs
@@ -4,6 +4,7 @@ using DotNet.Testcontainers.Containers;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Rusty.Template.Infrastructure.Database;
@@ -13,15 +14,27 @@ namespace Rusty.Template.Tests.Integration;
 
 public class WebApiFactory : WebApplicationFactory<IApiMarker>, IAsyncLifetime
 {
-    private readonly MsSqlTestcontainer _dbContainer = new TestcontainersBuilder<MsSqlTestcontainer>()
-        .WithDatabase(new MsSqlTestcontainerConfiguration("mcr.microsoft.com/mssql/server:2022-latest")
-        {
-            Password = "Test_Pass123"
-        }).Build();
+    private readonly MsSqlTestcontainer _dbContainer;
+    private readonly ConfigurationBuilder _configurationBuilder = new();
+    public WebApiFactory()
+    {
+        _configurationBuilder.AddJsonFile("appsettings.Staging.json", false, true);
+        var configurationRoot = _configurationBuilder.Build();
+        var dockerSection = configurationRoot.GetSection("Docker");
+        _dbContainer = new TestcontainersBuilder<MsSqlTestcontainer>()
+            .WithDatabase(new MsSqlTestcontainerConfiguration(dockerSection["Image"])
+            {
+                Password = dockerSection["Password"]
+            }).Build();
+    }
 
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         var connectionString = _dbContainer.ConnectionString + "TrustServerCertificate=True";
+        builder.ConfigureAppConfiguration(configurationBuilder =>
+        {
+            configurationBuilder.AddConfiguration(_configurationBuilder.Build());
+        });
         builder.ConfigureTestServices(services =>
         {
             services.RemoveAll(typeof(ConnectionStringFactory));
