8a3ec1e0a8a51c3c69b77a95496cf31ba4319616
RomanRusty <roman.yatskovyna@gmail.com>
2022-12-19 18:16:28 +0000

    Created bat

--- src/Rusty.Template.Infrastructure/Repositories/WeatherForecastRepo.cs
+++ src/Rusty.Template.Infrastructure/Repositories/WeatherForecastRepo.cs
@@ -1,4 +1,6 @@
+using System.Globalization;
 using Rusty.Template.Application.Repositories;
+using Rusty.Template.Contracts.Exceptions.Entity;
 using Rusty.Template.Domain;
 using Rusty.Template.Infrastructure.Database;
 using Rusty.Template.Infrastructure.Repositories.BaseRepository;
@@ -18,5 +20,20 @@ public class WeatherForecastRepo : BaseRepo<WeatherForecast>, IWeatherForecastRe
     /// <param name="context">The context</param>
     public WeatherForecastRepo(AppDbContext context) : base(context, item => item.Date)
     {
+
+  
+    }
+
+    /// <summary>
+    ///     Creates the no save using the specified entity
+    /// </summary>
+    /// <param name="entity">The entity</param>
+    /// <exception cref="EntityWitNameAlreadyExistsException{WeatherForecast}"></exception>
+    public override async Task CreateNoSaveAsync(WeatherForecast entity)
+    {
+        if (await ExistsAsync(item => item.Date == entity.Date))
+            throw new EntityWitNameAlreadyExistsException<WeatherForecast>(
+                entity.Date.ToString(CultureInfo.CurrentCulture));
+        await base.CreateNoSaveAsync(entity);
     }
 }
